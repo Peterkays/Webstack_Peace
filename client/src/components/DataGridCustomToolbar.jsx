9b863a03bfb7fedef6ac62428f2d0c62f5fe1267
Peter <peterkasinamunda@gmail.com>
2024-06-10 10:19:42 +0200

    adding the datagrid customer menu and adjusting the toolnar and the flexbetween file

--- client/src/components/DataGridCustomToolbar.jsx
+++ client/src/components/DataGridCustomToolbar.jsx
@@ -10,48 +10,29 @@ import {
 import FlexBetween from "./FlexBetween";
 
 const DataGridCustomToolbar = ({ searchInput, setSearchInput, setSearch }) => {
-  // Render the GridToolbarContainer
   return (
     <GridToolbarContainer>
-      // Render the FlexBetween component
       <FlexBetween width="100%">
-        // Render the FlexBetween component
         <FlexBetween>
-          // Render the GridToolbarColumnsButton
           <GridToolbarColumnsButton />
-          // Render the GridToolbarDensitySelector
           <GridToolbarDensitySelector />
-          // Render the GridToolbarExport
           <GridToolbarExport />
         </FlexBetween>
-        // Render the TextField component
         <TextField
-          // Set the label to "Search..."
           label="Search..."
-          // Set the styles for the TextField
           sx={{ mb: "0.5rem", width: "15rem" }}
-          // Set the onChange event handler to setSearchInput
           onChange={(e) => setSearchInput(e.target.value)}
-          // Set the value of the TextField to searchInput
           value={searchInput}
-          // Set the variant to "standard"
           variant="standard"
-          // Set the InputProps to include an endAdornment
           InputProps={{
-            // Set the endAdornment to include an IconButton
             endAdornment: (
               <InputAdornment position="end">
-                // Render the IconButton
                 <IconButton
-                  // Set the onClick event handler to setSearch
                   onClick={() => {
-                    // Call setSearch with the searchInput value
                     setSearch(searchInput);
-                    // Set setSearchInput to an empty string
                     setSearchInput("");
                   }}
                 >
-                  // Render the Search icon
                   <Search />
                 </IconButton>
               </InputAdornment>
---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/system";
+
+const FlexBetween = styled(Box)({
+  // Set the display property to "flex"
+  display: "flex",
+  // Set the justifyContent property to "space-between"
+  justifyContent: "space-between",
+  // Set the alignItems property to "center"
+  alignItems: "center",
+});
+
+export default FlexBetween;
